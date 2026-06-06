3c7e4e14606f34814499e7c664aac7e2a702c453
Guzmannrc <zfz9xchyc2@privaterelay.appleid.com>
2026-06-06 14:58:13 -0600

    Update videojuego.py

--- videojuego.py
+++ videojuego.py
@@ -33,19 +33,56 @@ class VideojuegoAhorcado:
 
     def pedir_letra(self):
 
-        letra= input("Ingresa una letra: ").lower()
-
+        letra = input("Ingresa una letra: ").lower()
+    
         if letra not in self.letras_adivinadas:
             self.letras_adivinadas.append(letra)
         else:
             print("Ya ingresaste esa letra")
 
+    def verificar_ganador(self):
+
+        for letra in self.palabra:
+
+            if letra not in self.letras_adivinadas:
+                return False
+        return True
+
+    def pedir_letra(self):
+
+        letra=input("Ingresa una letra:").lower()
+        
+        if letra not in self.letras_adivinadas:
+            self.letras_adivinadas.append(letra)
+            
+            if letra not in self.palabra:
+                self.intentos -= 1
+                print("Letra incorrecta")
+                print("Intentos resrantes:",self.intentos)
+        else:
+            print("Ya ingresaste esa letra")
+
+    def verificar_perdedor(self):
+        return self.intentos <=0
+
 juego = VideojuegoAhorcado()
 
-juego.mostrar_palabra()
+while True:
 
-juego.pedir_letra()
+    juego.mostrar_palabra()
 
-juego.mostrar_palabra()
+    juego.pedir_letra()
+
+    if juego.verificar_ganador():
+
+        print("Bn hecho!")
+        print("La palabra era:", juego.palabra)
+        break
+
+    if juego.verificar_perdedor():
+
+        print("Bot!")
+        print("La palabra era:", juego.palabra)
+        break
 
 
